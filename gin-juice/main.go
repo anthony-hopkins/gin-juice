23826ad52c01431bd501583c9f3218902c4d501e
Anthony hopkins <anthony.b.hopkins@gmail.com>
2023-09-19 16:14:09 -0600

    Update main.go
    Updated main.go to provide clarification via comments on functionality.

--- gin-juice/main.go
+++ gin-juice/main.go
@@ -10,15 +10,21 @@ func main() {
 	r := gin.Default()
 	r.Use(gin.Logger())
 
+	// Configure static resource location
 	r.Static("/vendor", "./static/vendor")
 
+	// Tell gin where our templates are stored. 
 	r.LoadHTMLGlob("templates/**/**")
 
+	// Set the default root route for the URL
 	r.GET("/", func(context *gin.Context) {
 		context.HTML(http.StatusOK, "views/index.html", gin.H{
 			"title": "Hello Gin!",
 		})
 	})
+	// More reoutes here baseds on strings/regex patterns
+
+	// Start the server providing a console confirmation
 	log.Println("Server started!")
 	r.Run() // Default port is 8080
 }
